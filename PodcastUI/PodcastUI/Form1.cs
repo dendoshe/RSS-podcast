aea9c88b002daf2291ab83457157c8a11a7b6636
dendoshe <diana.jumaili@gmail.com>
2018-10-24 14:15:02 +0200

    okänt2
    vet ej2

--- PodcastUI/PodcastUI/Form1.cs
+++ PodcastUI/PodcastUI/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using LogicLayer;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,8 @@ namespace PodcastUI
 {
     public partial class Form1 : Form
     {
+        PodcastMani logicLayer = new PodcastMani(); //Här har vi skapat ett object av PostcastMani som ligger i LogicLayer. Den körs en gång vid uppstart.
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +23,10 @@ namespace PodcastUI
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e) {
 
         }
     }
