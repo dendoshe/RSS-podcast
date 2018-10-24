f3119b6c400ed21131d05fc64b9eedfb27008c07
dendoshe <diana.jumaili@gmail.com>
2018-10-24 14:16:50 +0200

    okänt3
    vetej3

--- PodcastUI/PodcastUI/models/Podcasts.cs
+++ PodcastUI/PodcastUI/models/Podcasts.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace PodcastUI.models
 {
-    class Podcasts
+    public class Podcasts
     {
+        public string Url { get; set; }
+        public string Content { get; set; }
+        public int ContentLength
+        {
+            get { return Content != null ? Content.Length : 0; }
+        }
     }
+
+    public class PodcastList : List<Podcasts>
+    {
+       
+    }
+
+
+
 }
